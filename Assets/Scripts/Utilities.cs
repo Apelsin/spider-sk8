856b056fc2cdac2e09691476c33c67ee215d61f3
Apelsin <appelsindrikken@gmail.com>
2019-03-27 14:32:03 -0700

    trying to make parallax look nice

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public static class Utilities
 {
@@ -46,4 +47,10 @@ public static class Utilities
             return y;
         };
     }
+
+    [Serializable]
+    public class FloatEvent : UnityEvent<float> { }
+    [Serializable]
+    public class Vector3Event : UnityEvent<Vector3> { }
+
 }
