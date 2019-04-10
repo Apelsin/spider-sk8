dccafd9dc11c9fb18dc2bccc78d016a8889f52c0
Apelsin <appelsindrikken@gmail.com>
2019-04-10 04:45:46 -0700

    helper

---
+++ Assets/PPCanvas.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Canvas))]
+[RequireComponent(typeof(CanvasScaler))]
+public class PPCanvas : MonoBehaviour, ISerializationCallbackReceiver
+{
+    [SerializeField, HideInInspector]
+    private Canvas _Canvas;
+
+    public Canvas Canvas
+    {
+        get { return _Canvas; }
+    }
+
+    [SerializeField, HideInInspector]
+    private CanvasScaler _CanvasScaler;
+
+    public CanvasScaler CanvasScaler
+    {
+        get { return _CanvasScaler; }
+    }
+
+    private void LateUpdate()
+    {
+        var camera = Canvas.worldCamera;
+        if (camera == null)
+            camera = Camera.main;
+        var pp_cam = camera.GetComponent<PixelPerfectCamera>();
+        if (pp_cam)
+            CanvasScaler.scaleFactor = pp_cam.pixelRatio;
+    }
+
+    public void OnBeforeSerialize()
+    {
+        _Canvas = GetComponent<Canvas>();
+        _CanvasScaler = GetComponent<CanvasScaler>();
+    }
+
+    public void OnAfterDeserialize()
+    {
+    }
+}
